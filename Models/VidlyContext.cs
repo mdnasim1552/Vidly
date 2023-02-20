ad14adcc0a33a46bfed823b6d8278691036a511b
Md. Nasim Uddin <md.nasim1552@gmail.com>
2023-02-20 20:27:10 +0600

    Add project files.

---
+++ Models/VidlyContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class VidlyContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public VidlyContext()
+        :base("name=DefaultConnection")
+        {
+                
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            modelBuilder.Entity<MembershipType>()
+                .Property(m => m.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            modelBuilder.Entity<Genre>()
+                .Property(g => g.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            modelBuilder.Entity<Movie>()
+                .Property(m => m.GenreId)
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder); 
+        }
+    }
+}
